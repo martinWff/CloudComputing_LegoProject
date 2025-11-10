907d59728b532f853c0f4ac5d6aeba2f734eda33
Hoomanbeans <joaorf13@gmail.com>
2025-11-10 18:59:23 +0000

    .

--- RestAPI/src/main/java/com/example/legoproject/controllers/UserController.java
+++ RestAPI/src/main/java/com/example/legoproject/controllers/UserController.java
@@ -1,15 +1,21 @@
 package com.example.legoproject.controllers;
 
-import com.example.legoproject.models.User;
-import com.example.legoproject.models.UserProfile;
-import com.example.legoproject.services.UserService;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import com.example.legoproject.models.UserProfile;
+import com.example.legoproject.services.UserService;
 
 @RestController
 @RequestMapping("/users")
